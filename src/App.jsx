8efe0eadb94ca565f46d82c8aae5cd34a6a91bab
DiegoDiem <ldmg.9@hotmail.com>
2022-10-21 18:27:26 -0500

    si funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,28 @@
-import { useState, useEffect } from 'react'
-import Countdownlist from './components/Countdownlist'
+import { useState, useEffect } from 'react';
+import Countdown from './components/Countdownlist';
+
+import data from './data.json';
 
 function App() {
-
-  const [caracter1, useCaracter1] = useState()
-  const [caracter2, useCaracter2] = useState()
-  const [caracter3, useCaracter3] = useState()
-
-  const mes = "9"
-  const dia1 = "21"
-  const dia2 = "21"
-  const dia3 = "21"
-
-  const hora1 = "17"
-  const hora2 = "17"
-  const hora3 = "17"
-
-  const min1 = "46"
-  const min2 = "49"
-  const min3 = "55"
-
-  const [countdown, setCountdown] = useState(true)
-  const [countdown2, setCountdown2] = useState(false)
-  const [countdown3, setCountdown3] = useState(false)
-
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     const cotizarCripto = async () => {
+      setCharacter(data);
 
-      // useCaracters({})
-      const url = `https://rickandmortyapi.com/api/character`;
-      const respuesta = await fetch(url);
-      const resultado = await respuesta.json();
-      // console.log(resultado)
-      useCaracter1(resultado.results[0]);
-      useCaracter2(resultado.results[1]);
-      useCaracter3(resultado.results[2]);
+      // const url = `https://rickandmortyapi.com/api/character`;
+      // const respuesta = await fetch(url);
+      // const resultado = await respuesta.json();
+    };
 
-    }
-
-    cotizarCripto()
-
-  }, [])
-  console.log(caracter1)
-  console.log(caracter2)
-  console.log(caracter3)
-
-
+    cotizarCripto();
+  }, []);
+console.log(character);
   return (
-
-    <div >
-      {/* contador1 */}
-      {countdown ?
-        <Countdownlist
-          setCountdown={setCountdown}
-          setCountdown2={setCountdown2}
-          mes={mes}
-          dias={dia1}
-          horas={hora1}
-          minutos={min1}
-          name={caracter1?.name}
-          img={caracter1?.image}
-        /> : <></>}
-
-        {/* contador 2 */}
-      {countdown2 ?
-        <Countdownlist
-          setCountdown={setCountdown2}
-          setCountdown2={setCountdown3}
-          mes={mes}
-          dias={dia2}
-          horas={hora2}
-          minutos={min2}
-          name={caracter2?.name}
-          img={caracter2?.image}
-        /> : <></>}
-
-        {/* contador 3 */}
-      {countdown3 ?
-        <Countdownlist
-          setCountdown={setCountdown2}
-          setCountdown2={setCountdown3}
-          mes={mes}
-          dias={dia3}
-          horas={hora3}
-          minutos={min3}
-          name={caracter3?.name}
-          img={caracter3?.image}
-        /> : <></>}
+    <div>
+      <Countdown data={character} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Countdownlist.jsx
+++ src/components/Countdownlist.jsx
@@ -1,100 +1,90 @@
 /* eslint-disable prettier/prettier */
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
-const style={
-  displayNone:{
-    display:"none"
+const style = {
+  displayNone: {
+    display: 'none',
+  },
+};
+
+function Countdownlist({ data }) {
+  const [dia, setDias] = useState('00');
+  const [hora, setHoras] = useState('00');
+  const [minuto, setMinutos] = useState('00');
+  const [segundo, setSegundo] = useState('00');
+
+  const [count, setCount] = useState(0);
+  const current = data[count] || null;
+
+  useEffect(() => {
+    if (data[count] && data[count].date) {
+      const fecha = new Date(data[count].date);
+      executeCountTimer(fecha.getTime());
+
+      const intervalId = setInterval(() => {
+        executeCountTimer(fecha.getTime());
+      }, 1000);
+
+      return () => {
+        console.log('Clear.......');
+        clearInterval(intervalId);
+      };
+    }
+  }, [data,count]);
+
+  const executeCountTimer = (date) => {
+    const distance = date - new Date().getTime();
+    if (distance > 0) {
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setDias(days < 10 ? `0${days}` : days + '');
+      setHoras(hours < 10 ? `0${hours}` : hours + '');
+      setMinutos(minutes < 10 ? `0${minutes}` : minutes + '');
+      setSegundo(seconds < 10 ? `0${seconds}` : seconds + '');
+    } else {
+      setDias('00');
+      setHoras('00');
+      setMinutos('00');
+      setSegundo('00');
+
+      setCount((prev) => prev + 1);
+    }
+  };
+
+  if (!current) {
+    return '';
   }
-}
-
-
-function Countdownlist({setCountdown,setCountdown2, mes, dias, horas, minutos, name,img },) {
-
-
-    const [dia, setDias] = useState("0")
-    const [hora, setHoras] = useState("0")
-    const [minuto, setMinutos] = useState("0")
-    const [segundo, setSegundo] = useState("0")
-
-    useEffect(()=>{
-      // console.log(minutos);
-      let mesNum = parseInt(mes)
-      let diasNum = parseInt(dias)
-      let minutosNum = parseInt(minutos)
-      let horasNum = parseInt(horas)
-
-      // console.log(minutosNum);
-        //* Asignación de fecha en base a UTC +5 horas a la hora actual dividido por: año, mes-1, días, minutos, segundos
-        let countdownDate = Date.UTC(2022, mesNum, diasNum, horasNum, minutosNum, 0)
-        
-        // let countdownDate = new Date("May 19, 2022 16:00:00").getTime()
-        //? Actualiza cada seegundo
-        let distance;
-
-        let itv = setInterval( ()=> {
-           //? Dias y tiempo
-           let now = new Date().getTime() 
-
-           //? Tiempo entre ahora y el countdown establecido
-
-            distance = countdownDate - now;
-            // console.log(distance)
-
-           let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-           let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-           let minutes = Math.floor((distance% (1000 * 60 * 60)) / (1000 * 60))
-           let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-           
-
-          //  setTime(days + " : " + hours + " : " + minutes + " : " + seconds )
-
-           setDias(days+'')
-           setHoras(hours+'')
-           setMinutos(minutes+'')
-           setSegundo(seconds+'')
-
-           
-           if (distance < 0) {
-            
-              clearInterval(itv)
-              
-              setDias("")
-              setHoras("")
-              setMinutos("")
-              setSegundo("")
-              setCountdown(false)
-              setCountdown2(true)
-                     
-           }
-
-        },1000)
-
-    },[])
 
   return (
-    <div  style={dia==='' && hora==='' && minuto==='' && segundo===''? style.displayNone :  style.containerFlex} > 
+    <div
+      style={
+        dia === '' && hora === '' && minuto === '' && segundo === ''
+          ? style.displayNone
+          : style.containerFlex
+      }
+    >
       {/* <div style={style.div}>
         {time}
       </div>  */}
-      <img src={`${img}`} alt="" />
-      <p> {name} </p>
-      <div > 
-        <span >{dia}</span> : 
-        <span  >{hora}</span> : 
-        <span  >{minuto}</span> : 
-        <span >{segundo}</span>
+      <img src={current.image} alt="" />
+      <p> {current.name} </p>
+      <div>
+        <span>{dia}</span> :<span>{hora}</span> :<span>{minuto}</span> :
+        <span>{segundo}</span>
       </div>
-      <div  >
-        <span>Días</span> 
-        <span>Horas</span> 
-        <span >Minutos</span>
-        <span>Segundos</span> 
+      <div>
+        <span>Días</span>
+        <span>Horas</span>
+        <span>Minutos</span>
+        <span>Segundos</span>
       </div>
-      
     </div>
-  )
+  );
 }
 
-
-
-export default Countdownlist
+export default Countdownlist;
